f6bc70992ada4a1308b7964a8c732c8db36a573d
mryang113 <mryang113@163.com>
2020-05-12 09:19:06 +0800

    cart静态页面

--- src/pages/Cart/Cart.vue
+++ src/pages/Cart/Cart.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    Cart
+  <div id="cartContainer">
+    <div class="imgWrap">
+      <img src="./images/header.png" alt="">
+    </div>
+
+    <!-- 未登录内容区 -->
+    <div class="notLoginWrap">
+      <div class="title">
+        <span class="t-left">购物车</span>
+        <span class="t-right">领劵</span>
+      </div>
+      <div class="textInfo">
+        <ul>
+          <li>30天无忧退货</li>
+          <li>48小时快速退款</li>
+          <li>慢99元免邮费</li>
+        </ul>
+      </div>
+      <div class="cartImg">
+        <img src="./images/cart01.png" alt="">
+      </div>
+      <div class="login">登录</div>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -11,5 +35,6 @@
 </script>
 
 <style lang="stylus" scoped>
+  @import './Cart'
 
 </style>
